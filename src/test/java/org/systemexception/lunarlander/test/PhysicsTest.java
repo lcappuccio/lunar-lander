c05ec2149341a81a7df052259121987895c29e6b
Leonardo Cappuccio <leo@systemexception.org>
2015-12-28 22:23:12 +0100

    time is set to parameter

--- src/test/java/org/systemexception/lunarlander/test/PhysicsTest.java
+++ src/test/java/org/systemexception/lunarlander/test/PhysicsTest.java
@@ -3,6 +3,7 @@ package org.systemexception.lunarlander.test;
 import org.junit.Test;
 import org.systemexception.lunarlander.model.Physics;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,12 +19,17 @@ public class PhysicsTest {
 
 	@Test
 	public void run_simulation_physics() {
-		sut = new Physics();
+		sut = new Physics(6);
 		sut.runSimulation();
 		HashMap<Integer, List> data = sut.getData();
 		assertTrue(data.get(1).get(0).equals(9.8));
 		assertTrue(data.get(2).get(0).equals(19.6));
 		assertTrue(data.get(5).get(1).equals(122.5));
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void exception_on_negative_time() {
+		sut = new Physics(-1);
 	}
 
 }
