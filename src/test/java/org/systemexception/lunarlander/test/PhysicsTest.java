616aa7b6b727dada6538330fb3b42b6bd72785a3
Leonardo Cappuccio <leo@systemexception.org>
2015-12-28 23:06:39 +0100

    start from given height

--- src/test/java/org/systemexception/lunarlander/test/PhysicsTest.java
+++ src/test/java/org/systemexception/lunarlander/test/PhysicsTest.java
@@ -19,7 +19,7 @@ public class PhysicsTest {
 
 	@Test
 	public void run_simulation_physics() {
-		sut = new Physics(6);
+		sut = new Physics(100);
 		sut.runSimulation();
 		HashMap<Integer, List> data = sut.getData();
 		assertTrue(data.get(1).get(0).equals(9.8));
