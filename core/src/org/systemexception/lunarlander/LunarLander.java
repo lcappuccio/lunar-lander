363b52348189cc44ec3439bf83cb5f16cdf4b712
Leonardo Cappuccio <leo@systemexception.org>
2015-12-31 13:31:59 +0100

    to polygonspritebatch

--- core/src/org/systemexception/lunarlander/LunarLander.java
+++ core/src/org/systemexception/lunarlander/LunarLander.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -20,7 +20,7 @@ public class LunarLander extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private GameEngine gameEngine;
 
-	private SpriteBatch spritebatch;
+	private PolygonSpriteBatch batch;
 	private Box2DDebugRenderer debugRenderer;
 	private Matrix4 debugMatrix;
 	private BitmapFont font;
@@ -30,7 +30,7 @@ public class LunarLander extends ApplicationAdapter {
 
 	@Override
 	public void create() {
-		spritebatch = new SpriteBatch();
+		batch = new PolygonSpriteBatch();
 		soundThruster = Gdx.audio.newSound(Gdx.files.internal("thruster.ogg"));
 		soundRcs = Gdx.audio.newSound(Gdx.files.internal("rcs.ogg"));
 		gameEngine = new GameEngine(soundThruster, soundRcs);
@@ -54,18 +54,18 @@ public class LunarLander extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		gameEngine.logic();
-		spritebatch.setProjectionMatrix(camera.combined);
-		debugMatrix = spritebatch.getProjectionMatrix().cpy().scale(Dimensions.METERS_TO_PIXELS,
+		batch.setProjectionMatrix(camera.combined);
+		debugMatrix = batch.getProjectionMatrix().cpy().scale(Dimensions.METERS_TO_PIXELS,
 				Dimensions.METERS_TO_PIXELS, 0);
-		spritebatch.begin();
+		batch.begin();
 		Body body = gameEngine.getBodies().get(BodiesNames.BOX_BODY);
 		float v = (float) normalRelativeAngle(body.getAngle());
-		font.draw(spritebatch, "Angle: " + String.format("%.2f", v) + " deg", 20, 580);
-		font.draw(spritebatch, "Altitude: " + String.format("%.2f", body.getPosition().y) + " m", 20, 560);
-		font.draw(spritebatch, "H_Speed: " + String.format("%.2f", Math.abs(body.getLinearVelocity().x)) + " m/s", 20,
+		font.draw(batch, "Angle: " + String.format("%.2f", v) + " deg", 20, 580);
+		font.draw(batch, "Altitude: " + String.format("%.2f", body.getPosition().y) + " m", 20, 560);
+		font.draw(batch, "H_Speed: " + String.format("%.2f", Math.abs(body.getLinearVelocity().x)) + " m/s", 20,
 				540);
-		font.draw(spritebatch, "V_Speed: " + String.format("%.2f", body.getLinearVelocity().y) + " m/s", 20, 520);
-		spritebatch.end();
+		font.draw(batch, "V_Speed: " + String.format("%.2f", body.getLinearVelocity().y) + " m/s", 20, 520);
+		batch.end();
 		gameEngine.input();
 		debugRenderer.render(gameEngine.getWorld(), debugMatrix);
 	}
