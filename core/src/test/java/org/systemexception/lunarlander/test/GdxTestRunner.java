099be5c1e7c4a7d94194c9212aefb3e14cc75d60
Leonardo Cappuccio <leo@systemexception.org>
2016-01-02 03:31:18 +0100

    wip

---
+++ core/src/test/java/org/systemexception/lunarlander/test/GdxTestRunner.java
@@ -0,0 +1,8 @@
+package org.systemexception.lunarlander.test;
+
+/**
+ * @author leo
+ * @date 02/01/16 02:40
+ */
+public class GdxTestRunner {
+}
---
+++ core/src/test/java/org/systemexception/lunarlander/test/LunarLanderTest.java
@@ -0,0 +1,11 @@
+package org.systemexception.lunarlander.test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author leo
+ * @date 02/01/16 02:35
+ */
+public class LunarLanderTest {
+
+}
