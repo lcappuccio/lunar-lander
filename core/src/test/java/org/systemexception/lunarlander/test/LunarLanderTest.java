74cda2485ede1b587da6f29b1cd0d1ef6828377d
Leonardo Cappuccio <leo@systemexception.org>
2016-01-02 04:09:26 +0100

    wip test

--- core/src/test/java/org/systemexception/lunarlander/test/LunarLanderTest.java
+++ core/src/test/java/org/systemexception/lunarlander/test/LunarLanderTest.java
@@ -1,24 +1,37 @@
 package org.systemexception.lunarlander.test;
 
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Input;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.systemexception.lunarlander.LunarLander;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * @author leo
  * @date 02/01/16 02:35
  */
 @RunWith(GdxTestRunner.class)
-public class LunarLanderTest extends LunarLander {
+public class LunarLanderTest {
 
 	private LunarLander sut;
 
 	@Before
 	public void setUp() {
+		Gdx.graphics = mock(Graphics.class);
+		Gdx.audio = mock(Audio.class);
+		Gdx.gl20 = new MockGL20();
+		Gdx.input = mock(Input.class);
+		Gdx.gl = new MockGL20();
 	}
 
 	@Test
+	@Ignore
 	public void init() {
 		sut = new LunarLander();
 		sut.create();
